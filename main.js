9c9bb834a3def7ec72adafcd8da2198f1d040f7e
Matt <matthewky22@gmail.com>
2016-04-19 18:19:23 +0000

    Additions to js

--- main.js
+++ main.js
@@ -4,8 +4,25 @@ angular.module('toggle', []);
 angular.module('toggle')
     .controller('AppCtrl', ['$scope', function($scope) {
          $scope.custom = true;
-          $scope.toggleCustom = function () {
+            $scope.toggleCustom = function () {
         $scope.custom = !$scope.custom;
+    };
+    //Part 4
+        $scope.custom2 = true;
+            $scope.toggleCustom2 = function () {
+        $scope.custom2 = !$scope.custom2;
+    };     
+            $scope.changeColor = function () {
+        $scope.custom3 = !$scope.custom3;
     };
 }]);
     
+
+$scope.changeColor = function(person, bool) {
+    if(bool === true) {
+        $scope.personColour = {color: '#'+person.colour};
+    } else if (bool === false) {
+        $scope.personColour = {color: 'white'}; //or, whatever the original color is
+    }
+};
+
